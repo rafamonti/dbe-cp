09f172c11808767060f134819d4c09a9e905aa80
RAFAEL MONTI <rafamonti10@gmail.com>
2021-04-30 20:24:59 -0300

    uploading project

---
+++ dbe-cp/src/main/java/br/com/fiap/utils/JPAUtil.java
@@ -0,0 +1,16 @@
+package br.com.fiap.utils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public abstract class JPAUtil {
+	
+	public static EntityManager getEntityManager() {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("progamer-persistence-unit");
+		EntityManager manager = factory.createEntityManager();
+		return manager;
+	}
+	
+	
+}
